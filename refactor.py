ed9b2d1ddbe2ec4eeecb9c5852a5c4f3d15a2b21
JiyeonP <q0o0p@snu.ac.kr>
2022-11-16 19:27:21 +0900

    TODO3

--- refactor.py
+++ refactor.py
@@ -1,14 +1,10 @@
-def censorWord(word, text):
-    # tmp = text
-    # while word in tmp:
-    #     tmp = tmp[:tmp.find(word)] + "*" * len(word) + tmp[tmp.find(word) + len(word):]
-    # return text
-    return text.replace(word, "*")
-
-def alertText(text):
-    if (text.count("*") > 5):
-        print("More than five *")
-
+def censor_and_alert(text, *words):
+    for word in words:
+        text = text.replace(word, "*"*len(word))
+        if (text.count("*") > 5):
+            print("More than five *")
+    return text
+   
 if __name__ == "__main__":
     text = """Because he's the hero Gotham deserves but not the one it needs right now.
 So we will hunt him, because he can take it. Because he's not out hero.
@@ -18,11 +14,7 @@ He is a silent guardian, a watchful protector... a dark knight."""
     word2 = "silent"
 
     # TODO: 4. Consider general usage
-    # TODO: 3. Substitute algorithm
 
-    text = censorWord(word1, text)
-    alertText(text)
-    text = censorWord(word2, text)
-    alertText(text)
-
+    text = censor_and_alert(text, word1, word2)
+    
     print(text)
